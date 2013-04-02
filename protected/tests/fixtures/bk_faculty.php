2835b9234a5c8d041e00d7c0ac9d6b5a6e57b4a2
Kemal Maulana <kemskems12@gmail.com>
2013-04-02 23:09:41 +0700

    Membuat fixture untuk semua tabel pada basis data

---
+++ protected/tests/fixtures/bk_faculty.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'faculty1' => array(
+		'name' => 'Test Faculty 1',
+	),
+	'faculty2' => array(
+		'name' => 'Test Faculty 2',
+	),
+);
---
+++ protected/tests/fixtures/bk_student.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+	'student1' => array(
+		'username' => 'Test Student 1',
+		'photo' => '',
+		'is_admin' => 1,
+		'last_login_timestamp' => '',
+	),
+	'student2' => array(
+		'username' => 'Test Student 2',
+		'photo' => '',
+		'is_admin' => 0,
+		'last_login_timestamp' => '',
+	),
+	'student3' => array(
+		'username' => 'Test Student 3',
+		'photo' => '',
+		'is_admin' => 0,
+		'last_login_timestamp' => '',
+	),
+);
