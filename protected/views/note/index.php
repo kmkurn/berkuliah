b706ee5e2a2f659620246bb2c53268654f997c5a
Kemal Maulana <kemskems12@gmail.com>
2013-04-03 17:31:47 +0700

    Mengubah conroller dan view agar bisa melakukan pencarian pada halaman note/index

--- protected/views/note/index.php
+++ protected/views/note/index.php
@@ -14,6 +14,20 @@ $this->menu=array(
 
 <h1>Notes</h1>
 
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'search-form',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>128)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Cari'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
