009e847a62da15fca915d9924e64859f7d5e8842
Annisa Prida R <annisa.prida@gmail.com>
2013-04-04 19:17:25 +0700

    mengembalikan sitecontroller.php

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -77,32 +77,10 @@ class SiteController extends Controller
 	 */
 	public function actionLogin()
 	{
-<<<<<<< HEAD
 		$identity = new UserIdentity();
 		if($identity->authenticate())
-    		Yii::app()->user->login($identity);
-		$this->redirect(array('home/index'));
-=======
-		$model=new LoginForm;
-
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
->>>>>>> siteController
+		Yii::app()->user->login($identity);
+		$this->redirect(Yii::app()->homeUrl);
 	}
 
 	/**
