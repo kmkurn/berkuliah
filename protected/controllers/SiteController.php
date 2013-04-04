3d4e58de57e4dfa72a20a02ec77060db02f65d9d
Annisa Prida R <annisa.prida@gmail.com>
2013-04-04 18:41:02 +0700

    ubah redirect halaman index

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+		$this->render('berkuliah');
 	}
 
 	/**
@@ -77,10 +77,25 @@ class SiteController extends Controller
 	 */
 	public function actionLogin()
 	{
-		$identity = new UserIdentity();
-		if($identity->authenticate())
-    		Yii::app()->user->login($identity);
-		$this->redirect(Yii::app()->homeUrl);
+		$model=new LoginForm;
+
+		// if it is ajax validation request
+		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+
+		// collect user input data
+		if(isset($_POST['LoginForm']))
+		{
+			$model->attributes=$_POST['LoginForm'];
+			// validate user input and redirect to the previous page if valid
+			if($model->validate() && $model->login())
+				$this->redirect(Yii::app()->user->returnUrl);
+		}
+		// display the login form
+		$this->render('login',array('model'=>$model));
 	}
 
 	/**
