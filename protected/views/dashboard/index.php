a296324bf67f602838447b61452c9c65dfa88ff1
Kemal Maulana <kemskems12@gmail.com>
2013-04-09 19:25:48 +0700

    Menambah komentar pada view

--- protected/views/dashboard/index.php
+++ protected/views/dashboard/index.php
@@ -1,6 +1,7 @@
 <?php
 /* @var $this DashboardController */
 /* @var $downloadsDataProvider CArrayDataProvider */
+/* @var $uploadsDataProvider CActiveDataProvider */
 
 $this->pageTitle=Yii::app()->name . ' - Dasbor';
 
