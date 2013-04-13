2579d732231648019970bb91632d460b7df767dc
Kemal Maulana <kemskems12@gmail.com>
2013-04-13 14:56:25 +0700

    Mempercantik format URL

--- protected/config/main.php
+++ protected/config/main.php
@@ -35,7 +35,6 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -44,7 +43,6 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
