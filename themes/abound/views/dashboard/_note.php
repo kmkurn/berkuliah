acba134c7fb386cb3995e9d65d293b5ecdfa8497
Ashar Fuadi <fushar@gmail.com>
2013-04-14 00:07:18 +0700

    Mengubah format tampilan berkas pada sejarah pengunggahan

--- themes/abound/views/dashboard/_note.php
+++ themes/abound/views/dashboard/_note.php
@@ -7,21 +7,21 @@
 	
 	<table>
 		<tr>
-			<td>
+			<td width="40px">
 				<?php echo CHtml::image($data->getTypeIcon(), 'note icon', array('class' => 'note-icon','height'=>'30', 'width'=>'30')); ?>
 			</td>
 			<td>
-				<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
 				<?php echo CHtml::link(CHtml::encode($data->title), array('noteDetails/index', 'id'=>$data->id)); ?>
 				<br />
 
-				<b><?php echo CHtml::encode($data->getAttributeLabel('course_id')); ?>:</b>
-				<?php echo CHtml::encode($data->course->name); ?>
+				<i class="icon icon-briefcase"></i> <?php echo CHtml::encode($data->course->faculty->name); ?>
 				<br />
 
-				<b><?php echo CHtml::encode($data->getAttributeLabel('upload_timestamp')); ?>:</b>
+				<i class="icon icon-book"></i> <?php echo CHtml::encode($data->course->name); ?>
+				<br />
+
 				<?php // TO-DO: set locale ?>
-				<?php echo CHtml::encode(strftime('%A, %e %B %Y, %T', strtotime($data->upload_timestamp))); ?>
+				<i class="icon icon-time"></i> <?php echo CHtml::encode(strftime('%A, %e %B %Y, %T', strtotime($data->upload_timestamp))); ?>
 				<br />
 			</td>
 		</tr>
