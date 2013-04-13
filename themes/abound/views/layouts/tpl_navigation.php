226ad5cb2087d84c6937eeb92e85f6a8334794b7
Annisa Prida R <annisa.prida@gmail.com>
2013-04-13 11:04:26 +0700

    Mengubah default dan header

--- themes/abound/views/layouts/tpl_navigation.php
+++ themes/abound/views/layouts/tpl_navigation.php
@@ -26,7 +26,7 @@
       /*array('label'=>'Gii generated', 'url'=>array('customer/index')),*/
       array('visible'=>!Yii::app()->user->isGuest,'label'=>'<i class="icon icon-user icon-white"></i>'.Yii::app()->user->name, 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
         'items'=>array(
-         array('label'=>'<i class="icon icon-cog"></i>Pengaturan Foto',  'url'=>array('/site/page', 'view'=>'foto')),
+         array('label'=>'<i class="icon icon-cog"></i>Pengaturan Foto',  'url'=>array('dashboard/uploadPhoto')),
          array('label'=>'Logout', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
          )),
       array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest), 
@@ -39,10 +39,10 @@
 </div>
 </div>
 <div class="style-switcher pull-right">
- <a href="javascript:chooseStyle('none', 60)" checked="checked"><span class="style" style="background-color:#0578ED;"></span></a>
+ <a href="javascript:chooseStyle('none', 60)" checked="checked"><span class="style" style="background-color:#4e4e4e;"></span></a>
  <a href="javascript:chooseStyle('style2', 60)"><span class="style" style="background-color:#f5352e;"></span></a>
  <a href="javascript:chooseStyle('style3', 60)"><span class="style" style="background-color:#A0F80C;"></span></a>
- <a href="javascript:chooseStyle('style4', 60)"><span class="style" style="background-color:#4e4e4e;"></span></a>
+ <a href="javascript:chooseStyle('style4', 60)"><span class="style" style="background-color:#0578ED;"></span></a>
  <a href="javascript:chooseStyle('style5', 60)"><span class="style" style="background-color:#d85515;"></span></a>
  <a href="javascript:chooseStyle('style6', 60)"><span class="style" style="background-color:#a00a69;"></span></a>
  <a href="javascript:chooseStyle('style7', 60)"><span class="style" style="background-color:#a30c22;"></span></a>
