96328204e123365959df66cd85e85dde2f39f2cd
Ashar Fuadi <fushar@gmail.com>
2013-05-15 13:26:08 +0700

    Membetulkan script twitter bekuliah

--- themes/abound/views/site/index.php
+++ themes/abound/views/site/index.php
@@ -2,19 +2,6 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
-
-Yii::app()->clientScript->registerScript('twitter',
-  '!function(d,s,id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (!d.getElementById(id)) {
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "//platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js,fjs);
-    }
-  }(document,"script","twitter-wjs");'
-);
-
 ?>
 
 <blockquote class="pull-left">
@@ -43,11 +30,8 @@ Yii::app()->clientScript->registerScript('twitter',
         </td>
 
         <td>
-          <?php echo CHtml::link(CHtml::encode('Tweets by @berkuliah'), 'https://twitter.com/berkuliah', array(
-            'class' => 'twitter-timeline',
-            'data-dnt' => 'true',
-            'data-widget-id' => 321252419063390209,
-          )); ?>
+          <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/berkuliah" data-widget-id="321252419063390209">Tweets by @berkuliah</a>
+          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
         </td>
 
       </tr>
