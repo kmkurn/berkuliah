c75159cb06774cf6fb4ced2380a24f455a39043d
Ashar Fuadi <fushar@gmail.com>
2013-05-18 15:55:13 +0700

    Menambahkan daftar matkul Fasilkom pada skema di protected/data

--- protected/data/berkuliah.sql
+++ protected/data/berkuliah.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.1
+-- version 3.5.8.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 18, 2013 at 12:18 PM
--- Server version: 5.5.30-MariaDB-log
--- PHP Version: 5.4.15
+-- Generation Time: May 18, 2013 at 03:44 PM
+-- Server version: 5.5.31-0ubuntu0.13.04.1
+-- PHP Version: 5.4.9-4ubuntu2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `bk_badge`
 --
 
-DROP TABLE IF EXISTS `bk_badge`;
 CREATE TABLE IF NOT EXISTS `bk_badge` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
@@ -40,14 +39,126 @@ CREATE TABLE IF NOT EXISTS `bk_badge` (
 -- Table structure for table `bk_course`
 --
 
-DROP TABLE IF EXISTS `bk_course`;
 CREATE TABLE IF NOT EXISTS `bk_course` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `faculty_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `faculty_id` (`faculty_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=216 ;
+
+--
+-- Dumping data for table `bk_course`
+--
+
+INSERT INTO `bk_course` (`id`, `name`, `faculty_id`) VALUES
+(109, 'Administrasi Bisnis', 1),
+(110, 'Administrasi Sistem', 1),
+(111, 'Aljabar Linier', 1),
+(112, 'Aljabar Linier Numerik', 1),
+(113, 'Analisis Numerik', 1),
+(114, 'Aproksimasi & Sistem Nonlinier', 1),
+(115, 'Basis Data Lanjut', 1),
+(116, 'Bioinformatika', 1),
+(117, 'Business Intelligence', 1),
+(118, 'Computational Geometry', 1),
+(119, 'Customer Relationship Management', 1),
+(120, 'Dasar-Dasar Arsitektur Komputer', 1),
+(121, 'Dasar-Dasar Audit SI', 1),
+(122, 'Dasar-Dasar Pemrograman', 1),
+(123, 'Data Mining', 1),
+(124, 'Desain & Analisis Algoritma', 1),
+(125, 'E-Commerce', 1),
+(126, 'Embedded Systems', 1),
+(127, 'Enterprise Application Integration', 1),
+(128, 'Enterprise Resource Planning', 1),
+(129, 'Fisika Dasar 1', 1),
+(130, 'Fisika Dasar 2', 1),
+(131, 'Game Development', 1),
+(132, 'Geometric Modelling', 1),
+(133, 'Grafika Komputer', 1),
+(134, 'Infrastruktur TI Modern', 1),
+(135, 'Jaringan Komputer', 1),
+(136, 'Jaringan Komunikasi Data', 1),
+(137, 'Kerja Praktik', 1),
+(138, 'Komputasi Lunak', 1),
+(139, 'Komputer & Masyarakat', 1),
+(140, 'Komunikasi Bisnis dan Teknis', 1),
+(141, 'Konfigurasi ERP', 1),
+(142, 'Kriptografi & Keamanan Informasi', 1),
+(143, 'Layanan & Aplikasi Web', 1),
+(144, 'Logika Komputasional', 1),
+(145, 'Manajemen Keamanan Informasi', 1),
+(146, 'Manajemen Layanan TI', 1),
+(147, 'Manajemen Pengetahuan', 1),
+(148, 'Manajemen Proyek TI', 1),
+(149, 'Manajemen Sistem Informasi', 1),
+(150, 'Manajemen Sumber Daya Manusia', 1),
+(151, 'Matematika Dasar 1', 1),
+(152, 'Matematika Dasar 2', 1),
+(153, 'Matematika Diskret 1', 1),
+(154, 'Matematika Diskret 2', 1),
+(155, 'Metode Formal', 1),
+(156, 'Metodologi Penelitian & Penulisan Ilmiah', 1),
+(157, 'MPK Agama', 1),
+(158, 'MPK Bahasa Inggris', 1),
+(159, 'MPK Seni & Olahraga', 1),
+(160, 'MPKT A', 1),
+(161, 'MPKT B', 1),
+(162, 'Organisasi Sistem Komputer', 1),
+(163, 'Pemelajaran Mesin', 1),
+(164, 'Pemrograman Deklaratif', 1),
+(165, 'Pemrograman Konkuren & Parallel', 1),
+(166, 'Pemrograman Logika', 1),
+(167, 'Pemrograman Sistem', 1),
+(168, 'Pengajaran Berbantuan Komputer', 1),
+(169, 'Pengantar Organisasi Komputer', 1),
+(170, 'Pengantar Sistem Dijital', 1),
+(171, 'Pengembangan dan Pemasaran Produk', 1),
+(172, 'Pengembangan Perangkat Lunak Open Source', 1),
+(173, 'Pengolahan Bahasa Manusia', 1),
+(174, 'Pengolahan Citra', 1),
+(175, 'Pengolahan Multimedia', 1),
+(176, 'Pengolahan Sinyal Dijital', 1),
+(177, 'Penjaminan Mutu Perangkat Lunak', 1),
+(178, 'Penulisan Ilmiah', 1),
+(179, 'Perancangan & Pemrograman Web', 1),
+(180, 'Perilaku Organisasi', 1),
+(181, 'Perolehan Informasi', 1),
+(182, 'Persamaan Diferensial', 1),
+(183, 'Prinsip-Prinsip Manajemen', 1),
+(184, 'Prinsip-Prinsip Sistem Informasi', 1),
+(185, 'Proyek Pengembangan Sistem Informasi', 1),
+(186, 'Proyek Perangkat Lunak', 1),
+(187, 'Rancangan Sistem Dijital', 1),
+(188, 'Rekayasa Perangkat Lunak', 1),
+(189, 'Riset Operasi', 1),
+(190, 'Robotika', 1),
+(191, 'Semantic Web', 1),
+(192, 'Simulasi & Pemodelan', 1),
+(193, 'Sistem Cerdas', 1),
+(194, 'Sistem Informasi Akuntansi dan Keuangan', 1),
+(195, 'Sistem Informasi Geografis', 1),
+(196, 'Sistem Informasi Kesehatan', 1),
+(197, 'Sistem Interaksi', 1),
+(198, 'Sistem Operasi', 1),
+(199, 'Sistem Terdistribusi', 1),
+(200, 'Statistika & Probabilitas', 1),
+(201, 'Struktur Data & Algoritma', 1),
+(202, 'Supply Chain Management', 1),
+(203, 'Technopreneurship', 1),
+(204, 'Teknik Kompilator', 1),
+(205, 'Teknologi Mobile', 1),
+(206, 'Teori Bahasa & Automata', 1),
+(207, 'Teori Informasi', 1),
+(208, 'Topik Khusus Arsitektur & Infrastruktur', 1),
+(209, 'Topik Khusus Bidang Minat Enterprise System', 1),
+(210, 'Topik Khusus Bidang Minat Teknologi Informasi', 1),
+(211, 'Topik Khusus Kecerdasan Komputasional', 1),
+(212, 'Topik Khusus Pengolahan Informasi Multimedia', 1),
+(213, 'Topik Khusus Teknologi Perangkat Lunak', 1),
+(214, 'Tugas Akhir', 1),
+(215, 'Ubiquitous & Net-Centric Computing', 1);
 
 -- --------------------------------------------------------
 
@@ -55,7 +166,6 @@ CREATE TABLE IF NOT EXISTS `bk_course` (
 -- Table structure for table `bk_download_info`
 --
 
-DROP TABLE IF EXISTS `bk_download_info`;
 CREATE TABLE IF NOT EXISTS `bk_download_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `student_id` int(11) NOT NULL,
@@ -72,12 +182,18 @@ CREATE TABLE IF NOT EXISTS `bk_download_info` (
 -- Table structure for table `bk_faculty`
 --
 
-DROP TABLE IF EXISTS `bk_faculty`;
 CREATE TABLE IF NOT EXISTS `bk_faculty` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `bk_faculty`
+--
+
+INSERT INTO `bk_faculty` (`id`, `name`) VALUES
+(1, 'Fakultas Ilmu Komputer');
 
 -- --------------------------------------------------------
 
@@ -85,7 +201,6 @@ CREATE TABLE IF NOT EXISTS `bk_faculty` (
 -- Table structure for table `bk_note`
 --
 
-DROP TABLE IF EXISTS `bk_note`;
 CREATE TABLE IF NOT EXISTS `bk_note` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
@@ -107,7 +222,6 @@ CREATE TABLE IF NOT EXISTS `bk_note` (
 -- Table structure for table `bk_rate`
 --
 
-DROP TABLE IF EXISTS `bk_rate`;
 CREATE TABLE IF NOT EXISTS `bk_rate` (
   `student_id` int(11) NOT NULL,
   `note_id` int(11) NOT NULL,
@@ -123,7 +237,6 @@ CREATE TABLE IF NOT EXISTS `bk_rate` (
 -- Table structure for table `bk_report`
 --
 
-DROP TABLE IF EXISTS `bk_report`;
 CREATE TABLE IF NOT EXISTS `bk_report` (
   `student_id` int(11) NOT NULL,
   `note_id` int(11) NOT NULL,
@@ -138,7 +251,6 @@ CREATE TABLE IF NOT EXISTS `bk_report` (
 -- Table structure for table `bk_review`
 --
 
-DROP TABLE IF EXISTS `bk_review`;
 CREATE TABLE IF NOT EXISTS `bk_review` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
@@ -156,7 +268,6 @@ CREATE TABLE IF NOT EXISTS `bk_review` (
 -- Table structure for table `bk_student`
 --
 
-DROP TABLE IF EXISTS `bk_student`;
 CREATE TABLE IF NOT EXISTS `bk_student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) NOT NULL,
@@ -174,7 +285,6 @@ CREATE TABLE IF NOT EXISTS `bk_student` (
 -- Table structure for table `bk_student_badge`
 --
 
-DROP TABLE IF EXISTS `bk_student_badge`;
 CREATE TABLE IF NOT EXISTS `bk_student_badge` (
   `student_id` int(11) NOT NULL,
   `badge_id` int(11) NOT NULL,
@@ -188,7 +298,6 @@ CREATE TABLE IF NOT EXISTS `bk_student_badge` (
 -- Table structure for table `bk_testimonial`
 --
 
-DROP TABLE IF EXISTS `bk_testimonial`;
 CREATE TABLE IF NOT EXISTS `bk_testimonial` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
@@ -256,3 +365,7 @@ ALTER TABLE `bk_student_badge`
 --
 ALTER TABLE `bk_testimonial`
   ADD CONSTRAINT `bk_testimonial_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `bk_student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
