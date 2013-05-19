50f92345d821da4c651c67016912b97b2b71a079
Kemal Maulana <kemskems12@gmail.com>
2013-05-19 16:19:35 +0700

    Memperbaiki fixture untuk tabel bk_student

--- protected/tests/fixtures/bk_student.php
+++ protected/tests/fixtures/bk_student.php
@@ -2,21 +2,30 @@
 
 return array(
 	'student1'=>array(
-		'username'=>'Test Student 1',
-		'photo'=>'',
+		'username'=>'test1',
+		'name'=>'Test Student 1',
+		'bio'=>'Test bio 1.',
+		'photo'=>'test1.jpg',
 		'is_admin'=>1,
 		'last_login_timestamp'=>date('Y-m-d H:i:s'),
+		'faculty_id'=>1,
 	),
 	'student2'=>array(
-		'username'=>'Test Student 2',
-		'photo'=>'',
+		'username'=>'test2',
+		'name'=>'Test Student 2',
+		'bio'=>'Test bio 2.',
+		'photo'=>'test2.png',
 		'is_admin'=>0,
 		'last_login_timestamp'=>date('Y-m-d H:i:s'),
+		'faculty_id'=>1,
 	),
 	'student3'=>array(
-		'username'=>'Test Student 3',
-		'photo'=>'',
+		'username'=>'test3',
+		'name'=>'Test Student 3',
+		'bio'=>'Test bio 3.',
+		'photo'=>'test3.jpeg',
 		'is_admin'=>0,
 		'last_login_timestamp'=>date('Y-m-d H:i:s'),
+		'faculty_id'=>2,
 	),
 );
