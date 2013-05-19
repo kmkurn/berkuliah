bf9e6cb5633c899aa5757546be9351e83898a46c
Kemal Maulana <kemskems12@gmail.com>
2013-05-19 10:28:29 +0700

    Memperbaiki fixtures

--- protected/tests/fixtures/bk_course.php
+++ protected/tests/fixtures/bk_course.php
@@ -1,16 +1,16 @@
 <?php
 
 return array(
-	'course1' => array(
-		'name' => 'Test Course 1',
-		'faculty_id' => 1,
+	'course1'=>array(
+		'name'=>'Test Course 1',
+		'faculty_id'=>1,
 	),
-	'course2' => array(
-		'name' => 'Test Course 2',
-		'faculty_id' => 1,
+	'course2'=>array(
+		'name'=>'Test Course 2',
+		'faculty_id'=>1,
 	),
-	'course3' => array(
-		'name' => 'Test Course 3',
-		'faculty_id' => 2,
+	'course3'=>array(
+		'name'=>'Test Course 3',
+		'faculty_id'=>2,
 	),
 );
--- protected/tests/fixtures/bk_note.php
+++ protected/tests/fixtures/bk_note.php
@@ -1,34 +1,31 @@
 <?php
 
 return array(
-	'note1' => array(
-		'title' => 'Test Note 1',
-		'description' => 'This is a test note 1 description.',
-		'type' => 0,
-		'location' => '',
-		'course_id' => 1,
-		'student_id' => 1,
-		'upload_timestamp' => '',
-		'edit_timestamp' => '',
+	'note1'=>array(
+		'title'=>'Test Note 1',
+		'description'=>'This is a test note 1 description.',
+		'type'=>0,
+		'course_id'=>1,
+		'student_id'=>1,
+		'upload_timestamp'=>date('Y-m-d H:i:s'),
+		'edit_timestamp'=>date('Y-m-d H:i:s'),
 	),
-	'note2' => array(
-		'title' => 'Test Note 2',
-		'description' => 'This is a test note 2 description.',
-		'type' => 1,
-		'location' => '',
-		'course_id' => 2,
-		'student_id' => 2,
-		'upload_timestamp' => '',
-		'edit_timestamp' => '',
+	'note2'=>array(
+		'title'=>'Test Note 2',
+		'description'=>'This is a test note 2 description.',
+		'type'=>1,
+		'course_id'=>2,
+		'student_id'=>2,
+		'upload_timestamp'=>date('Y-m-d H:i:s'),
+		'edit_timestamp'=>'',
 	),
-	'note3' => array(
-		'title' => 'Test Note 3',
-		'description' => 'This is a test note 3 description.',
-		'type' => 2,
-		'location' => '',
-		'course_id' => 3,
-		'student_id' => 2,
-		'upload_timestamp' => '',
-		'edit_timestamp' => '',
+	'note3'=>array(
+		'title'=>'Test Note 3',
+		'description'=>'This is a test note 3 description.',
+		'type'=>2,
+		'course_id'=>3,
+		'student_id'=>2,
+		'upload_timestamp'=>date('Y-m-d H:i:s'),
+		'edit_timestamp'=>date('Y-m-d H:i:s'),
 	),
 );
