57274c4e547d16e564808d8b9abe6d7d0102df6a
Annisa Prida R <annisa.prida@gmail.com>
2013-05-20 18:15:44 +0700

    Customize Profil Pengguna

--- protected/views/student/_profile.php
+++ protected/views/student/_profile.php
@@ -1,33 +1,39 @@
+<html>
 <?php
 /* @var $this StudentController */
 /* @var $model Student */
-
 ?>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		array(
-			'label'=>$model->getAttributeLabel('uploadedPhoto'),
-			'value'=>Yii::app()->baseUrl . '/photos/' . (($model->photo == null) ? 'user.png' : $model->photo),
-			'type'=>'image',
-			'visible'=>(Yii::app()->user->id !== $model->id),
+<body>
+<div class = "profilPengguna">
+	<?php $this->widget('zii.widgets.CDetailView', array(
+		'cssFile' => Yii::app()->theme->baseUrl .'css/abound.css',
+		'data'=>$model,
+		'attributes'=>array(
+			array(
+				'label'=>'<i class="icon icon-camera"></i>'.$model->getAttributeLabel('uploadedPhoto'),
+				'value'=>Yii::app()->baseUrl . '/photos/' . (($model->photo == null) ? 'user.png' : $model->photo),
+				'type'=>'image',
+				'visible'=>(Yii::app()->user->id !== $model->id),
+			),
+			'username:text:Username JUITA',
+			'name',
+			array(
+				'label'=>'<i class="icon icon-book"></i>'.$model->getAttributeLabel('faculty_id'),
+				'value'=>(($model->faculty === null) ? null : CHtml::encode($model->faculty->name)),
+				'visible'=>$model->faculty !== null,
+			),
+			array(
+				'label'=>'<i class="icon icon-pencil"></i>'.$model->getAttributeLabel('bio'),
+				'value'=>$model->bio,
+				'visible'=>$model->bio !== null,
+			),
+			array(
+				'label'=>'<i class="icon icon-time"></i>'.$model->getAttributeLabel('last_login_timestamp'),
+				'value'=>Yii::app()->format->datetime($model->last_login_timestamp),
+			),
 		),
-		'username:text:Username JUITA',
-		'name',
-		array(
-			'label'=>$model->getAttributeLabel('faculty_id'),
-			'value'=>(($model->faculty === null) ? null : CHtml::encode($model->faculty->name)),
-			'visible'=>$model->faculty !== null,
-		),
-		array(
-			'label'=>$model->getAttributeLabel('bio'),
-			'value'=>$model->bio,
-			'visible'=>$model->bio !== null,
-		),
-		array(
-			'label'=>$model->getAttributeLabel('last_login_timestamp'),
-			'value'=>Yii::app()->format->datetime($model->last_login_timestamp),
-		),
-	),
-)); ?>
+	)); 
+	?>
+</div>
+</body>
+</html>
