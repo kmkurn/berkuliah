c4b9b0201a1a30a8ce4a321814a0a5cd1949fa4a
Kemal Maulana <kemskems12@gmail.com>
2013-05-21 19:06:14 +0700

    Memperbaiki indentasi dan minor bug

--- protected/views/student/_profile.php
+++ protected/views/student/_profile.php
@@ -1,66 +1,71 @@
-<html>
 <?php
 /* @var $this StudentController */
 /* @var $model Student */
 
 ?>
 
-<body>
-	<div class="row-fluid">
-	<div class="span12">	
+<div class="row-fluid">
+	<div class="span12">
+
 	<?php $this->beginWidget('zii.widgets.CPortlet', array(
 		'title'=>'<i class="icon icon-user"></i> <strong>PROFIL ' .$model->name . '</strong>',
-		)); 
-	?>
-	<table class="table table-hover"><tr><th rowspan="6">
-	<?php
-	$photo = ($data->student->photo === null) ? 'user.png' : $model->uploadedPhoto;
-	echo CHtml::image(Yii::app()->baseUrl . '/photos/' . $photo, 'fotoku', array("width"=>200));
-	?>
-	</th>
-	</tr>
-	<td>
-	<i class="icon icon-align-justify"></i>username
-	</td>
-	<td>:
-	</td>
-	<td><?php echo CHtml::encode($model->username);?></td>
-	<tr>
-	<td>
-	<i class="icon icon-user"></i>Nama
-	</td>
-	<td>:
-	</td>
-	<td><?php echo CHtml::encode($model->name);?></td>
-	</tr>
-	<tr>
-	<td>
-	<i class="icon icon-book"></i>Fakultas
-	</td>
-	<td>:
-	</td>
-	<td><?php echo CHtml::encode($model->faculty->name);?></td>
-	</tr>
-	<tr>
-	<td>
-	<i class="icon icon-pencil"></i>Bio
-	</td>
-	<td>:
-	</td>
-	<td><?php echo CHtml::encode($model->bio);?></td>
-	</tr>
-	<tr>
-	<td>
-	<i class="icon icon-time"></i>Terakhir Masuk
-	</td>
-	<td>:
-	</td>
-	<td><?php echo CHtml::encode($model->last_login_timestamp);?></td>
-	</tr>
-</table>
-		<?php $this->endWidget(); ?>
-	
+	)); ?>
+
+		<table class="table table-hover">
+
+			<tr>
+				<th rowspan="6">
+				<?php
+				$photo = ($model->photo === null) ? 'user.png' : $model->photo;
+				echo CHtml::image(Yii::app()->baseUrl . '/photos/' . $photo, CHtml::encode($model->name),
+					array('width'=>200));
+				?>
+				</th>
+			</tr>
+
+			<tr>
+				<td>
+					<i class="icon icon-align-justify"></i><?php echo $model->getAttributeLabel('username'); ?>
+				</td>
+				<td>:</td>
+				<td><?php echo CHtml::encode($model->username); ?></td>
+			</tr>
+
+			<tr>
+				<td>
+					<i class="icon icon-user"></i><?php echo $model->getAttributeLabel('name'); ?>
+				</td>
+				<td>:</td>
+				<td><?php echo CHtml::encode($model->name); ?></td>
+			</tr>
+
+			<tr>
+				<td>
+					<i class="icon icon-book"></i><?php echo $model->getAttributeLabel('faculty_id'); ?>
+				</td>
+				<td>:</td>
+				<td><?php echo CHtml::encode($model->faculty->name); ?></td>
+			</tr>
+
+			<tr>
+				<td>
+					<i class="icon icon-pencil"></i><?php echo $model->getAttributeLabel('bio'); ?>
+				</td>
+				<td>:</td>
+				<td><?php echo CHtml::encode($model->bio); ?></td>
+			</tr>
+
+			<tr>
+				<td>
+					<i class="icon icon-time"></i><?php echo $model->getAttributeLabel('last_login_timestamp'); ?>
+				</td>
+				<td>:</td>
+				<td><?php echo Yii::app()->format->datetime($model->last_login_timestamp); ?></td>
+			</tr>
+
+		</table>
+
+	<?php $this->endWidget(); ?>
+
+	</div>
 </div>
-</div>
-</body>
-</html>
