7b2428709daf22ac762717ae86f4c9d1ae08f73e
Kemal Maulana <kemskems12@gmail.com>
2013-05-21 21:38:12 +0700

    Mengimplementasikan terima dan tolak testimonial oleh admin (closes #102)

--- protected/controllers/TestimonialController.php
+++ protected/controllers/TestimonialController.php
@@ -10,8 +10,9 @@ class TestimonialController extends Controller
 		return array(
 			'accessControl', // perform access control for viewing articles details
 			'checkTestimonialOwner + propose, update', // check user before proposing a testimonial
-			'checkAdmin + grant', // check user before granting a testimonial
-			'checkStatus + update', // check status before updating a testimonial
+			'checkAdmin + grant, approve, reject', // check user before granting, approving, or rejecting a testimonial
+			'checkNewStatus + update', // check status before updating a testimonial
+			'checkPendingStatus + approve, reject', // check status before approving or rejecting a testimonial
 		);
 	}
 
@@ -19,7 +20,7 @@ class TestimonialController extends Controller
 	{
 		return array(
 			array('allow', // allow authenticated user
-				'actions'=>array('propose', 'grant', 'view', 'update'),
+				'actions'=>array('propose', 'grant', 'view', 'update', 'approve', 'reject'),
 				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
@@ -121,6 +122,50 @@ class TestimonialController extends Controller
 	}
 
 	/**
+	 * Approves a testimonial.
+	 * @param  integer $id the testimonial id
+	 */
+	public function actionApprove($id)
+	{
+		$model = $this->loadModel($id);
+
+		if (!$model->approve())
+		{
+			Yii::app()->user->setFlash('message', 'Testimoni gagal diterima.');
+			Yii::app()->user->setFlash('messageType', 'danger');
+		}
+		else
+		{
+			Yii::app()->user->setFlash('message', 'Testimoni berhasil diterima.');
+			Yii::app()->user->setFlash('messageType', 'success');
+		}
+
+		$this->redirect(array('view', 'id'=>$model->id));
+	}
+
+	/**
+	 * Rejects a testimonial.
+	 * @param  integer $id the testimonial id
+	 */
+	public function actionReject($id)
+	{
+		$model = $this->loadModel($id);
+
+		if (!$model->reject())
+		{
+			Yii::app()->user->setFlash('message', 'Testimoni gagal ditolak.');
+			Yii::app()->user->setFlash('messageType', 'danger');
+		}
+		else
+		{
+			Yii::app()->user->setFlash('message', 'Testimoni berhasil ditolak.');
+			Yii::app()->user->setFlash('messageType', 'success');
+		}
+
+		$this->redirect(array('view', 'id'=>$model->id));
+	}
+
+	/**
 	 * Loads the testimonial model.
 	 * @param  int $id the testimonial id
 	 * @return notethe testimonial object associated with the given id
@@ -166,14 +211,36 @@ class TestimonialController extends Controller
 	 * A filter to assure only new testimonial can be updated.
 	 * @param  CFilterChain $filterChain the filter chain
 	 */
-	public function filterCheckStatus($filterChain)
+	public function filterCheckNewStatus($filterChain)
 	{
 		if (isset($_GET['id']))
 		{
 			$model = $this->loadModel($_GET['id']);
-			if ($model->status != Testimonial::STATUS_NEW)
+			$status = Testimonial::STATUS_NEW;
+			if ($model->status != $status)
 			{
-				throw new CHttpException(403, 'Testimoni ini statusnya bukan baru.');
+				$statusMap = Testimonial::getStatusMap();
+				throw new CHttpException(403, 'Testimoni ini statusnya bukan "'.$statusMap[$status].'".');
+			}
+		}
+
+		$filterChain->run();
+	}
+
+	/**
+	 * A filter to assure only pending testimonial can be approved or rejected.
+	 * @param  CFilterChain $filterChain the filter chain
+	 */
+	public function filterCheckPendingStatus($filterChain)
+	{
+		if (isset($_GET['id']))
+		{
+			$model = $this->loadModel($_GET['id']);
+			$status = Testimonial::STATUS_PENDING;
+			if ($model->status != $status)
+			{
+				$statusMap = Testimonial::getStatusMap();
+				throw new CHttpException(403, 'Testimoni ini statusnya bukan "'.$statusMap[$status].'".');
 			}
 		}
 
