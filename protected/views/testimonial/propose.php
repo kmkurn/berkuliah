a72274b679a19bb07a3826da29a2c637faedb71b
mercia noviana <mercia_a@yahoo.com>
2013-05-21 07:10:17 +0700

    Menulis artikel penghargaan propose

---
+++ protected/views/testimonial/propose.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this TestimonialController */
+/* @var $model Testimonial */
+
+$this->breadcrumbs=array(
+	'Usulkan Artikel',
+);
+
+?>
+
+<div class="page-header"></div>
+
+<div class="row-fluid">
+	<div class="span9">
+		<?php $this->renderPartial('_proposeForm', array('model'=>$model)); ?>
+	</div><!-- span9 -->
+</div><!-- row-fluid -->
