0ee45a9f6bcd05427552397d6c5d52f8340305fe
mercia noviana <mercia_a@yahoo.com>
2013-05-23 20:44:34 +0700

    Naikin blocked quote artikel penghargaan

--- protected/views/site/_testimonial.php
+++ protected/views/site/_testimonial.php
@@ -1,20 +1,31 @@
-<?php
-/* @var $this SiteController */
-/* @var $model Testimonial */
+	<?php
+	/* @var $this SiteController */
+	/* @var $model Testimonial */
 
-?>
-
-			<div id="fotoArtikel">
-				<?php
-				$photo = ($model->student->photo === null) ? 'user.png' : $model->student->photo;
-				echo CHtml::image(Yii::app()->baseUrl . '/photos/' . $photo, $model->student->name, array('width'=>'100','align'=>'left',));
-				?>
-			</div>
-						<p><?php echo $model->content; ?></p>
+	?>
+		<table class="table">
+			<tbody class="penulisArtikel">
+				<tr>
+				<td>
+				<div id="fotoArtikel">
+					<?php
+					$photo = ($model->student->photo === null) ? 'user.png' : $model->student->photo;
+					echo CHtml::image(Yii::app()->baseUrl . '/photos/' . $photo, $model->student->name, array('width'=>'300','align'=>'left',));
+					?>
+				</div>
+					<p><?php echo $model->content; ?></p>
 					<br />
-					<blockquote class="pull-right">
-						<p><small>Oleh 
-			<?php echo CHtml::link(CHtml::encode($model->student->name), array('student/view', 'id'=>$model->student_id)); ?>
-			<br />
-			<?php echo Yii::app()->format->datetime($model->timestamp); ?></small></p>
-		</blockquote>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+				<blockquote class="pull-right">
+					<p><small>Oleh 
+						<?php echo CHtml::link(CHtml::encode($model->student->name), array('student/view', 'id'=>$model->student_id)); ?>
+						<br />
+						<?php echo Yii::app()->format->datetime($model->timestamp); ?></small></p>
+				</blockquote>
+				</td>
+			</tr></tbody>
+		</table>
