a28f027e0bc16379ed0c880472944b288172fbbd
Kemal Maulana <kemskems12@gmail.com>
2013-05-26 02:59:49 +0700

    Quick fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,7 +2,10 @@
 
 # Installs dependencies for testing via Travis-CI
 
-git clone https://github.com/yiisoft/yii.git
+wget http://yii.googlecode.com/files/yii-1.1.13.e9e4a0.tar.gz
+tar xvf yii-1.1.13.e9e4a0.tar.gz
+mv yii-1.1.13.e9e4a0/ yii/
+
 pear channel-discover pear.phpunit.de
 pear install phpunit/PHP_Invoker
 pear install phpunit/PHPUnit_Selenium
