56b2e9970f64a301fe59969259d9f7206d21e7aa
Kemal Maulana <kemskems12@gmail.com>
2013-05-25 22:31:59 +0700

    Mengubah icon sejarah pelaporan menjadi flag

--- protected/views/student/_report.php
+++ protected/views/student/_report.php
@@ -7,7 +7,7 @@
 <table class="table table-hover">
 	<tr>
 		<td>
-		<i class="icon icon-star"></i> Melaporkan berkas 
+		<i class="icon icon-flag"></i> Melaporkan berkas 
 		<?php echo CHtml::link(CHtml::encode($data['title']), array('note/view', 'id'=>$data['note_id'])); ?> 
 		pada tanggal 
 		<?php echo Yii::app()->format->datetime($data['timestamp']); ?>
