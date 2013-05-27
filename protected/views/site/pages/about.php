d206e7beb6c04c0e97c7799b38596d409cc56775
mercia noviana <mercia_a@yahoo.com>
2013-05-27 11:23:47 +0700

    About baru

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -19,48 +19,42 @@ Yii::app()->clientScript->registerScript('carousel',
 <div class="page-header"></div>
 
 <div class="row-fluid">
-  <div class="span7">
-    <div id="this-carousel-id" class="carousel slide">
-      <div class="carousel-inner">
-
-        <div class="item active">
-          <a href=""><?php echo CHtml::image(Yii::app()->baseUrl . '/images/header.png','header'); ?></a>
-        </div><!-- item active -->
-
-        <div class="item">
-          <a href=""><?php echo CHtml::image(Yii::app()->baseUrl . '/images/kemal.png','Kemal Maulana K'); ?></a>
-          <div class="carousel-caption">
-            <p>Kemal Maulana Kurniawan 1006666040</p>
-          </div><!-- carousel-caption -->
-        </div><!-- item -->
-
-        <div class="item">
-          <a href=""><?php echo CHtml::image(Yii::app()->baseUrl . '/images/ashar.png','Ashar Fuadi'); ?></a>
-          <div class="carousel-caption">
-            <p>Ashar Fuadi 1006666721</p>
-          </div><!-- carousel-caption -->
-        </div><!-- item -->
-
-        <div class="item">
-          <a href=""><?php echo CHtml::image(Yii::app()->baseUrl . '/images/merci.png','Mercia'); ?></a>
-          <div class="carousel-caption">
-            <p>Mercia 1006770740</p>
-          </div><!-- carousel-caption -->
-        </div><!-- item -->
-
-        <div class="item">
-          <a href=""><?php echo CHtml::image(Yii::app()->baseUrl . '/images/icha.png',' '); ?></a>
-          <div class="carousel-caption">
-            <p>Annisa Prida Rachmadianty 1006671192</p>
-          </div><!-- carousel-caption -->
-        </div><!-- item -->
-
-      </div><!-- .carousel-inner -->
-      
-      <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
-      <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
-
-    </div><!-- .carousel -->
+  <div class="span9">
+      <div class="team">
+        <h3> Tim BerKuliah: </h3>
+        <div class="developer">
+          <div class="team-photo">
+            <a href="https://twitter.com/kemaalmaulana"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/kemal2.png','Kemal'); ?></a>
+            <h6> Kemaal Maulana </h6>
+            <h6> Project Manager </h6>
+          </div>
+          <div class="team-photo">
+            <a href="https://twitter.com/fushar"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/ashar2.png','Ashar'); ?></a>
+            <h6> Ashar Fuadi </h6>
+            <h6> Back-End Programmer </h6>
+          </div>
+          <div class="team-photo">
+            <a href="https://twitter.com/annisaprida"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/icha2.png','Annisa'); ?></a>
+          <h6> Annisa Prida </h6>
+            <h6> Front-End Programmer </h6>
+          </div>
+          <div class="team-photo">
+            <a href="https://twitter.com/mercianov"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/merci2.png','Mercia'); ?></a>
+          <h6> Mercia </h6>
+            <h6> User Interface Designer </h6>
+          </div>
+        </div>
+        <div style="clear:both"></div>
+          <h4> Terima kasih kepada: </h4>
+         <div class="thanks-to"> 
+          <div class="team-photo">
+          <a href="https://twitter.com/bagofair"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/dhika.jpg','Dhika'); ?></a>
+            <h6> Dhika Ahmad Aulia </h6>
+            <h6> Logo Designer </h6>
+          </div>
+        </div>
+      </div>
+        
 
   </div><!-- span7 -->
 </div><!-- row-fluid -->
