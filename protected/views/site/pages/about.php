2cd7558157ec405e3be812de656d02aa177b92b6
mercia noviana <mercia_a@yahoo.com>
2013-06-07 15:23:10 +0700

    about lagi
    yey

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -20,8 +20,40 @@ Yii::app()->clientScript->registerScript('carousel',
 
 <div class="row-fluid">
   <div class="span9">
-      <div class="team">
-        <h3> Tim BerKuliah: </h3>
+    <div class="content-about">
+        
+       <ol> <li> <a href="#about-berkuliah">  Tentang Berkulliah </a></li>
+       <li><a href="#team">Tim Kami</a></li></ol>
+      
+        </div>
+       <div id="about-berkuliah">
+        <h3 class="title-about">Tentang BerKuliah</h3>
+        
+        <div class="teks-about">
+          <p>
+              BerKuliah.com merupakan sebuah sistem informasi untuk berbagi berkas kuliah. Berkas kuliah yang 
+              dimaksud dapat berupa catatan materi kuliah maupun arsip soal ujian, baik dalam format PDF, 
+              gambar, maupun teks. Aplikasi ini diperuntukkan bagi pemilik akun JUITA (Jaringan UI Terpadu). 
+          </p>
+          <p>
+              Pada umumnya, jika seorang mahasiswa membutuhkan berkas kuliah mahasiswa lain, maka ia akan 
+              memfotokopinya. Dengan adanya aplikasi ini, pengguna dapat mengunduh dan mempelajari berkas 
+              kuliah yang telah diunggah pengguna lain secara digital. Dengan demikian, diharapkan kertasTkertas 
+              fotokopi berkas kuliah yang sering dihasilkan mahasiswa dapat berkurang secara signifikan dan 
+              penghematan kertas besarTbesaran dapat tercapai. 
+          </p>
+          <p>
+              Melalui aplikasi ini pula, pengguna dapat mencari berkas kuliah yang dibuat pengguna lain dengan 
+              cepat dan mudah. Mahasiswa pada umumnya lebih memahami materi kuliah yang diterangkan oleh 
+              sesama mahasiwa. Dengan demikian, aplikasi ini dapat membantu pembelajaran para pengguna. 
+          </p>
+        </div>
+        <br />
+      </div>
+    <!---->
+    <br/>
+      <div id="team">
+        <h3> Tim BerKuliah</h3>
         <div class="developer">
           <div class="team-photo">
             <a href="https://twitter.com/kemaalmaulana"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/kemal2.png','Kemal'); ?></a>
