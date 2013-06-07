722179121b657bbaaff75f74ba6bdb6640ff2cc3
Ashar Fuadi <fushar@gmail.com>
2013-06-07 14:23:45 +0700

    Membetulkan footer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -46,14 +46,25 @@
         <div class="subnav navbar navbar-fixed-bottom">
             <div class="navbar-inner-footer">
                 <div class="container">
-                  <div class="pull-left"><h7><?php echo CHtml::link('Ketentuan Layanan', array('site/page', 'view'=>'tos')); ?></h7></div>
-                    <div id="logo-tengah"><?php echo CHtml::image(Yii::app()->request->baseUrl . '/images/icons/logo-berkuliah.png','logo', array('width' => '30', 'height'=>'30',));?></div>
-                    <div class="pull-right">&copy; <?php echo date('Y'); ?>
-                    <h7><?php echo CHtml::link('Kelompok C3', array('site/page', 'view'=>'about')); ?></h7>.
-                    All Rights Reserved.</div>
-                    <br/>
-                    <div id="tengah"><?php echo Yii::powered(); ?></div>
+
+                  <div id="footer-left">
+                      <?php echo CHtml::link('Ketentuan Layanan', array('site/page', 'view'=>'tos')); ?>
+                      <b>&middot;</b>
+                      <?php echo CHtml::link('Tentang Kami', array('site/page', 'view'=>'about')); ?>
+                  </div>
+
+                  <div id="footer-middle">
+                      <?php echo CHtml::image(Yii::app()->request->baseUrl . '/images/icons/logo-berkuliah.png','logo', array('width' => '30', 'height'=>'30',));?>
+                      <br />
+                      <?php echo Yii::powered(); ?>
+                  </div>
+
+                  <div id="footer-right">
+                      Hak Cipta &copy; <?php echo date('Y'); ?> BerKuliah
+                  </div>
+                 
                 </div><!-- container -->
+
             </div><!-- navbar-inner-footer -->
         </div><!-- subnav navbar navbar-fixed-bottom -->
     </footer>
